5afbe084ae48bbd28c0731534e8dd45f5103fd44
ArielArroyo69 <54905552+ArielArroyo69@users.noreply.github.com>
2019-09-11 12:37:02 -0400

    Add files via upload

---
+++ Test.java
@@ -0,0 +1,34 @@
+/**
+ * @author Ariel Arroyo
+ **/
+public class Test {
+	public static void main(String[] args) {
+//			System.out.println(78+98); //176
+//			System.out.println('a'+'b'); //195
+//			System.out.println('a'-'b'); //-1
+//			System.out.println('0'+'0'); //96
+//			System.out.println('a'+1); //98
+//			System.out.println('a'); //a
+			
+			int num = 10;
+			double dollars = 15.60;
+			String name1 = "Pablo";
+			String name2 = "Juan";
+			int nug = 50;
+			String name3 = "Ariel";
+			String nugs = "Chicken Nuggets";
+			
+			System.out.printf("I have %d cars",  num); //I have 10 cars
+			System.out.printf(" %nMaria has %.3f in her pocket", dollars);
+			
+			System.out.printf("%n%s is %d years older than %s", name1, num, name2);
+			
+			
+			System.out.printf("%n%s bought %d %s cause he was hungry.", name3, nug, nugs);
+			
+			
+			
+
+	}
+
+}
