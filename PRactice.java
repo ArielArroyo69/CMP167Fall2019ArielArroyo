177ac3c8f8b3168b7d79c238e6d98f215d09b149
ArielArroyo69 <54905552+ArielArroyo69@users.noreply.github.com>
2019-10-07 12:39:55 -0400

    Add files via upload

---
+++ PRactice.java
@@ -0,0 +1,21 @@
+
+public class PRactice {
+	public static void main(String[] args) {
+		int product = productThree(45, 70, 27);
+		System.out.println(product);
+		
+		int sum = sumTwo(20, 9);
+		System.out.println(sum);
+	}
+	
+	public static int productThree(int a, int b, int c) {
+		return a*b*c;
+	}
+	
+	public static int sumTwo(int n, int m) {
+		return n+m;
+	}
+
+
+
+}
